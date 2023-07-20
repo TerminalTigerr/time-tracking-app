4a3f16af439042f4e5dc36ded102d4e4b6e65102
BitHero <nuhumusah874@gmail.com>
2023-07-20 00:46:12 +0000

    inverse data flow

--- public/js/app.js
+++ public/js/app.js
@@ -17,6 +17,16 @@ class TimerDashboard extends React.Component {
       }
     ]
   }
+  handleCreateFormSubmit = (timer) => {
+    this.createTimer(timer)
+  }
+
+  createTimer = (timer) => {
+    const t = helpers.newTimer(timer)
+    this.setState({
+      timers: this.state.timers.concat(t)
+    })
+  }
   render() {
     return (
       <div className='ui three column centered grid'>
@@ -24,7 +34,9 @@ class TimerDashboard extends React.Component {
           <EditableTimerList 
             timers={this.state.timers}
           />
-          <ToggleableTimerForm />
+          <ToggleableTimerForm 
+            onFormSubmit={this.handleCreateFormSubmit}
+          />
         </div>
       </div>
     )
@@ -39,10 +51,21 @@ class ToggleableTimerForm extends React.Component {
   handleFormOpen = () => {
     this.setState({ isOpen: true })
   }
+  handleFormClose = () => {
+    this.setState({ isOpen: false })
+  }
+  handleFormSubmit = (timer) => {
+    this.props.onFormSubmit(timer)
+    this.setState({isOpen: false})
+  }
+
   render() {
     if(this.state.isOpen) {
       return(
-        <TimerForm />
+        <TimerForm 
+          onFormSubmit={this.handleFormSubmit}
+          onFormClose={this.handleFormClose}
+        />
       )
     } else {
       return(
@@ -154,9 +177,16 @@ class TimerForm extends React.Component {
   handleProjectChange = (e) => {
     this.setState({project: e.target.value})
   }
+  handleSubmit = () => {
+    this.props.onFormSubmit({
+      id: this.props.id,
+      title: this.state.title,
+      project: this.state.project
+    })
+  }
 
   render() {
-    const submitText = this.props.title ? 'Update' : 'Create'
+    const submitText = this.props.id ? 'Update' : 'Create'
     return (
       <div className='ui centered card'>
         <div className='content'>
@@ -166,20 +196,24 @@ class TimerForm extends React.Component {
               <input 
                 type='text' 
                 value={this.state.title}
-                OnChange={this.handleTitleChange} />
+                onChange={this.handleTitleChange} />
             </div>
             <div className='field'>
               <label>Project</label>
               <input 
                 type='text' 
                 value={this.state.project}
-                OnChange={this.handleProjectChange} />
+                onChange={this.handleProjectChange} />
             </div>
             <div className='ui two bottom attached buttons'>
-              <button className='ui basic blue button'>
+              <button 
+                className='ui basic blue button'
+                onClick={this.handleSubmit}>
                 {submitText}
               </button>
-              <button className='ui basic red button'>
+              <button 
+                className='ui basic red button'
+                onClick={this.props.onFormClose}>
                 Cancel
               </button>
             </div>
